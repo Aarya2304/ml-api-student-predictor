10d31d836473e6be88dd90998cc17cd1c28b65f8
Aarya2304 <aarya.yadav@mitwpu.edu.in>
2026-04-09 18:23:12 +0530

    Initial commit - training model setup

---
+++ train.py
@@ -0,0 +1,21 @@
+import numpy
+import pandas
+from sklearn.linear_model import LinearRegression
+import pickle
+
+data = {
+    "hours": [1,2,3,4,5,6,7,8],
+    "attendance": [50,60,65,70,75,80,85,90],
+    "score": [40,50,55,60,65,70,75,85]
+}
+
+df = pandas.DataFrame(data)
+
+X = df[["hours", "attendance"]]
+y = df[["score"]]
+
+model = LinearRegression()
+model.fit(X, y)
+
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
